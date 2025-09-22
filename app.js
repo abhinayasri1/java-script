6a59139f9d83d13f7b0f6f637a3d46d1f2069c28
Abhinaya <abhinayayasani@gmail.com>
2025-09-22 11:01:55 +0530

    data type

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-var a=67;
-var a=90;
-let b=68;
-let b=54;
-const c=78;
-const c=45;
-console.log(`value b is ${b}`);
-console.log(a);
+// var a=67;
+// var a=90;
+// const c=78;
+// console.log(`value b is ${b}`);
+// console.log(a);
+
+a=567;
+console.log(typeof a);
+name="sanjay";
+console.log(typeof name);
+c=true;
+console.log(typeof c);
+Date=null;
+console.log(typeof Date);
+d=undefined;
+console.log(typeof d);
+// non primitive data types
+Array=[65,"sanjay",true,null];
+console.log(typeof Array);
+obj={name:"sanjay",age:23};
+console.log(typeof data);
